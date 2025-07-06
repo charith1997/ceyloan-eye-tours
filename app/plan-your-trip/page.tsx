392b2cd4f55d220c412f7c3512a38a4e551b9c06
charith1997 <charithmadu97@gmail.com>
2025-07-06 17:28:06 +0530

    Fixed

--- app/plan-your-trip/page.tsx
+++ app/plan-your-trip/page.tsx
@@ -39,7 +39,7 @@ export default function PlanYourTrip() {
           industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
+          five centuries, but also the leap into typesetting,
           remaining essentially unchanged.
         </p>
       </div>
