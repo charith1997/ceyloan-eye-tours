cfbcd21d2903a3603b9aff38fd4db475d69687a9
charithaventude <charith.j@aventudent.com>
2025-12-14 16:16:16 +0530

    Revert "version upgraded"
    This reverts commit 99e8b37f5ea5bc2a408cb9a1757e153112805caf.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,26 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "react/display-name": "off",
-    "@next/next/no-img-element": "off",
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react/display-name": "off",
+      "@next/next/no-img-element": "off",
+    },
   },
-}, {
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}];
+];
 
 export default eslintConfig;
